d89c58367f213b9cda16bb458538d8bb181cf1f6
kungfuchrissy <40478183+kungfuchrissy@users.noreply.github.com>
2019-11-12 00:29:10 -0500

    Add files via upload

---
+++ RestaurantApp/RestaurantView.swift
@@ -0,0 +1,44 @@
+//
+//  RestaurantView.swift
+//  RestaurantApp
+//
+//  Created by chris on 9/5/19.
+//  Copyright © 2019 chris. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantView: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension RestaurantView: UITableViewDataSource,UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
